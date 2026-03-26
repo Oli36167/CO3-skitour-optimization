c4a1d602deacc749029e7c6c371a535c921837fd
liviavinz <vinzeliv@students.zhaw.ch>
2026-03-26 10:16:49 +0100

    Add files via upload

---
+++ ski_tour.py
@@ -0,0 +1,29 @@
+
+"""
+Ski tour coordinates (xmin, ymin, xmax, ymax) in LV03
+Times in HOURS
+"""
+# Ski tour 1
+start_xy_1 = (781801.00, 205189.00)
+goal_xy_1 = (784498.00, 205762.00)
+time_1 = 3.5
+
+# Ski tour 2
+start_xy_2 = (781801.00, 205189.00)
+goal_xy_2 = (785951.00, 206477.00)
+time_2 = 3.5
+
+# Ski tour 3
+start_xy_3 = (781801.00, 205189.00)
+goal_xy_3 = (782552.00, 209643.00)
+time_3 = 4
+
+# Ski tour 4
+start_xy_4 = (781801.00, 205189.00)
+goal_xy_4 = (781906.00, 203536.00)
+time_4 = 2
+
+# Ski tour 5
+start_xy_5 = (781801.00, 205189.00)
+goal_xy_5 = (783604.99, 201205.00)
+time_5 = 4
