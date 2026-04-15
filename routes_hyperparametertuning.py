10464bd188b4ba0c38d7c658cb537f1bfe0a811e
LiviaVi <vinzens.livia@bluewin.ch>
2026-04-15 20:01:16 +0200

    new routes for hyperparameter tuning

---
+++ routes_hyperparametertuning.py
@@ -0,0 +1,46 @@
+# Routes for hyperparameter tuning
+
+routes = [
+        (
+            "Sulzfluh",
+            terrain.coords_to_rowcol(781802.1, 205187.5),
+            terrain.coords_to_rowcol(782548, 209637),
+        ),
+        (
+            "Rotspitz",
+            terrain.coords_to_rowcol(781802.1, 205187.5),
+            terrain.coords_to_rowcol(785947.6, 206472.9),
+        ),
+        (
+            "Älpeltispitz",
+            terrain.coords_to_rowcol(785694.6, 195408.8),
+            terrain.coords_to_rowcol(789559.8, 195614.5),
+        ),
+        (
+            "Hoch Ducan",
+            terrain.coords_to_rowcol(784308, 177225.3),
+            terrain.coords_to_rowcol(784541.1, 173761.1),
+        ),
+        (
+            "Sandhubel (von Arosa)",
+            terrain.coords_to_rowcol(771448.9, 183255.6),
+            terrain.coords_to_rowcol(771613.5, 179002.9),
+        ),
+        (
+            "Schiesshorn",
+            terrain.coords_to_rowcol(771448.9, 183255.6),
+            terrain.coords_to_rowcol(772958.5, 181964),
+        ),
+        (
+            "Mattjisch Horn",
+            terrain.coords_to_rowcol(773501.7, 187988.8),
+            terrain.coords_to_rowcol(774243.3, 191425.3),
+        ),
+        (
+            "Sentisch Hora",
+            terrain.coords_to_rowcol(789401.9, 184754.7),
+            terrain.coords_to_rowcol(789150.1, 182406),
+        )
+    ]
+
+
